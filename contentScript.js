fa5d881e574003641599b56b9491a8ae2e0b10c4
nataliekeys <nataliekeys412@gmail.com>
2023-02-15 12:52:00 -0800

    hsl function added

--- contentScript.js
+++ contentScript.js
@@ -36,7 +36,7 @@ function getColor(x, y) {
 	let red = pixel[0];
 	let green = pixel[1];
 	let blue = pixel[2];
-	let hex = rgbToHex(red, green, blue)
+	let hex = rgbToHsl(red, green, blue)
 
 	// To-Do: turn hex into color name
 	console.log("Hex: ", hex)
@@ -74,6 +74,39 @@ function rgbToHex(r, g, b) {
     }
 
     return "#" + hexCode;
+}
+
+function rgbToHsl(r, g, b) {
+  r /= 255;
+  g /= 255;
+  b /= 255;
+  let cmin = Math.min(r,g,b),
+      cmax = Math.max(r,g,b),
+      delta = cmax - cmin,
+      h = 0,
+      s = 0,
+      l = 0;
+  if (delta == 0)
+    h = 0;
+  else if (cmax == r)
+    h = ((g - b) / delta) % 6;
+  else if (cmax == g)
+    h = (b - r) / delta + 2;
+  else
+    h = (r - g) / delta + 4;
+
+  h = Math.round(h * 60);
+    
+  if (h < 0)
+      h += 360;
+	 l = (cmax + cmin) / 2;
+
+  s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+    
+  s = +(s * 100).toFixed(1);
+  l = +(l * 100).toFixed(1);
+
+  return "hsl(" + h + "," + s + "%," + l + "%)";
 }
 
 
