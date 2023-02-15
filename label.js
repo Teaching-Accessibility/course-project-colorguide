aeb120a4aadb1264d9ab4c4e8063cffa966b4431
Tin Ngo <64702845+tin159iop@users.noreply.github.com>
2023-02-14 22:00:47 -0800

    Finish minimize/maximize func of labels

--- label.js
+++ label.js
@@ -5,30 +5,26 @@ function createLabel(top, left, content) {
     
     newLabel.style.position = 'absolute';
     newLabel.style.top = top;
-		//newLabel.style.textAlign = "center";
     newLabel.style.left = left;
     newLabel.style.zIndex = "99";
     newLabel.style.background = "#ffffff";
 	newLabel.style.cursor  = "pointer";
+	newLabel.style.display = "block"
 
-	// newLabel.innerHTML = content;
     newLabel.id = "ColorLabel" + count;
 	newLabel.className = "colorlabel";
     count ++;
-    // newLabel.style.height = "50%";
-    // newLabel.style.width = "180px";
-				 addMinimizeButton(newLabel);
-    addCloseButton(newLabel);
 
+
+	let newMinimizedLabel = createMinimizedLabel (top, left, newLabel);
+
+
+	addMinimizeButton(newLabel, () => {toggleDiv(newMinimizedLabel, newLabel)});
+    addCloseButton(newLabel);
 	addContent(newLabel, content);
 
-
-	newLabel.onclick = () => {
-        // newLabel.parentNode.removeChild(newLabel);
-		console.log ("Hello there")
-    };
-
 	document.body.appendChild(newLabel);
+	document.body.appendChild(newMinimizedLabel);
 }
 
 function addCloseButton(label) {
@@ -47,19 +43,16 @@ function addCloseButton(label) {
 	label.appendChild(closeButton);
 }
 
-function addMinimizeButton(label) {
+function addMinimizeButton(label, func) {
 	let minimizeButton = document.createElement("p");
-	 minimizeButton.style.float = "left";
-	 minimizeButton.style.display ="inline-flex";
-	
-	 minimizeButton.style.color = "#000000";
-	 minimizeButton.className = "colorlabel_child";
-	 minimizeButton.onclick = () => {
-        // minimize functionality here
-    };
+	minimizeButton.style.float = "left";
+	minimizeButton.style.display ="inline-flex";
+	minimizeButton.style.color = "#000000";
+	minimizeButton.className = "colorlabel_child";
+	minimizeButton.onclick = func
 
 	let  minimizeSymbol = document.createTextNode("-");
-	 minimizeButton.appendChild( minimizeSymbol);
+	minimizeButton.appendChild( minimizeSymbol);
 
 	label.appendChild( minimizeButton);
 }
@@ -72,3 +65,43 @@ function addContent (label, content) {
 	textContent.style.color = "#000000";
 	label.appendChild(textContent);
 }
+
+function createMinimizedLabel (top, left, label) {
+	let newMinimizedLabel = document.createElement('div');
+	newMinimizedLabel.style.position = 'absolute';
+    newMinimizedLabel.style.top = top;
+    newMinimizedLabel.style.left = left;
+    newMinimizedLabel.style.zIndex = "99";
+    newMinimizedLabel.style.background = "red";
+	newMinimizedLabel.style.cursor  = "pointer";
+	newMinimizedLabel.style.display = "none"
+	newMinimizedLabel.className = "colorlabel_child";
+
+	let maximizeButton = document.createElement("p");
+	maximizeButton.style.float = "left";
+	maximizeButton.style.display ="inline-flex";
+	maximizeButton.style.color = "#000000";
+	maximizeButton.className = "colorlabel_child";
+	maximizeButton.onclick = () => {toggleDiv(newMinimizedLabel, label)}
+
+	let  maximizeSymbol = document.createTextNode("+");
+	maximizeButton.appendChild(maximizeSymbol);
+
+	newMinimizedLabel.appendChild(maximizeButton);
+
+	return newMinimizedLabel
+}
+
+function toggleDiv(div1, div2)
+{
+	if(div1.style.display == 'none')
+	{
+		div2.style.display = 'none';
+		div1.style.display = 'block';
+	}
+	else
+	{  
+		div1.style.display = 'none';
+		div2.style.display = 'block'
+	}
+} 
