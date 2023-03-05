828dc6a64ee17c96b33d74e4d05d2ece00cdc42f
Tin Ngo <64702845+tin159iop@users.noreply.github.com>
2023-03-05 15:54:45 -0800

    Moved minimized button

--- content/label.js
+++ content/label.js
@@ -9,7 +9,7 @@ function createLabel(x, y, content) {
 
     newLabel.style.position = 'absolute';
     newLabel.style.zIndex = "999999999999997";
-		newLabel.style.borderRadius = "4px";
+	newLabel.style.borderRadius = "4px";
     newLabel.style.background = "#ffffff";
     newLabel.style.cursor  = "pointer";
     newLabel.style.display = "block"
@@ -21,51 +21,38 @@ function createLabel(x, y, content) {
     newLabel.className = "colorlabel";
 
 	newLabelPosition = labelPosition (x, y, newLabel.style.width, newLabel.style.height)
-	let maximizeX= x;
-	let maximizeY = y;
-	// All the +5 -5 are = (size of circle lable) /2
+
+	const circleLabelSize = 12
 	switch (newLabelPosition) {
 		case 'top left':
-			newLabel.style.top = x - parseInt(newLabel.style.height) - 5 + "px";
-			newLabel.style.left =  y - parseInt(newLabel.style.width) - 5 + "px";
-			addCircle(newLabel, parseInt(newLabel.style.height) + "px",  parseInt(newLabel.style.width) + "px");
-			maximizeX=  parseInt(newLabel.style.height) + "px";
-			maximizeY=   parseInt(newLabel.style.width) + "px";
+			newLabel.style.top = x - parseInt(newLabel.style.height) - circleLabelSize/2 + "px";
+			newLabel.style.left =  y - parseInt(newLabel.style.width) - circleLabelSize/2 + "px";
+			addCircle(newLabel, parseInt(newLabel.style.height) + "px",  parseInt(newLabel.style.width) + "px", circleLabelSize);
 			break;
 		case 'top right':
-			newLabel.style.top = x - parseInt(newLabel.style.height) - 5 + "px";
-			newLabel.style.left = y + 5 + "px";
-			addCircle(newLabel, parseInt(newLabel.style.height) + "px", "-11.25px");
-			maximizeX=  parseInt(newLabel.style.height) + "px";
-			maximizeY= "-11.25px";
-			break;
+			newLabel.style.top = x - parseInt(newLabel.style.height) - circleLabelSize/2 + "px";
+			newLabel.style.left = y + circleLabelSize/2 + "px";
+			addCircle(newLabel, parseInt(newLabel.style.height) + "px", -circleLabelSize + "px", circleLabelSize);
 			break;
 		case 'bottom left':
-			newLabel.style.top = x + 5 + "px";
-			newLabel.style.left = y - parseInt(newLabel.style.width) - 5 + "px";
-			addCircle(newLabel, "-11.25px", parseInt(newLabel.style.width) + "px");
-			maximizeX=  "-11.25px";
-			maximizeY= parseInt(newLabel.style.width) + "px";
+			newLabel.style.top = x + circleLabelSize/2 + "px";
+			newLabel.style.left = y - parseInt(newLabel.style.width) - circleLabelSize/2 + "px";
+			addCircle(newLabel, -circleLabelSize + "px", parseInt(newLabel.style.width) + "px", circleLabelSize);
 			break;
 		case 'bottom right':
-			newLabel.style.top = x + 5 + "px";
-			newLabel.style.left = y + 5 + "px";
-			addCircle(newLabel, "-11.25px", "-11.25px");
-			maximizeX= "-11.25px";
-			maximizeY= "-11.25px";
+			newLabel.style.top = x + circleLabelSize/2 + "px";
+			newLabel.style.left = y + circleLabelSize/2 + "px";
+			addCircle(newLabel, -circleLabelSize + "px", -circleLabelSize + "px", circleLabelSize);
 			break;
 		default:
 			console.log(`Something wrong with label position`);
 	}
 
-
-
-
 	addMinimizeButton(newLabel, () => {toggleLabel(newMinimizedLabel, newLabel)});
 	addCloseButton(newLabel);
 	addContent(newLabel, content);
 	document.body.appendChild(newLabel);
-	let newMinimizedLabel = createMinimizedLabel (maximizeX, maximizeY, newLabel);
+	let newMinimizedLabel = createMinimizedLabel (x, y, newLabel);
 	document.body.appendChild(newMinimizedLabel);
 }
 
@@ -93,8 +80,6 @@ function labelPosition(top, left, width, height) {
 		result += "right"
 	}
 
-
-
 	return result
 }
 
@@ -102,9 +87,9 @@ function addCloseButton(label) {
 	let closeButton = document.createElement("p");
 
 	closeButton.style.float = "right";
-	 closeButton.style.position = "absolute";
-  closeButton.style.right = 0 +'px';
-  closeButton.style.top = 0 +'px';
+	closeButton.style.position = "absolute";
+	closeButton.style.right = 0 +'px';
+	closeButton.style.top = 0 +'px';
 	closeButton.style.display ="inline";
 	closeButton.style.color = "#000000";
 	closeButton.style.margin = "auto";
@@ -132,8 +117,8 @@ function addMinimizeButton(label, func) {
 
 	minimizeButton.style.float = "left";
 	minimizeButton.style.position = "absolute";
-  minimizeButton.style.left = 0 +'px';
-  minimizeButton.style.top = -1 +'px';
+	minimizeButton.style.left = 0 +'px';
+	minimizeButton.style.top = -1 +'px';
 	minimizeButton.style.display ="inline";
 	minimizeButton.style.color = "#000000";
 	minimizeButton.style.margin = "auto";
@@ -154,17 +139,15 @@ function addMinimizeButton(label, func) {
 	label.appendChild( minimizeButton);
 }
 
-function addCircle(label, top, left) {
-	console.log(`top: ${top}, left: ${left}`);
+function addCircle(label, top, left, size) {
 	let circle = document.createElement("p");
 	circle.style.position = 'relative';
 	circle.style.margin = "0px";
-
-  circle.style.top = top;
-  circle.style.left = left;
-  circle.style.zIndex = "999999999999999";
-	circle.style.height = "11.5px";
-	circle.style.width = "11.5px";
+    circle.style.top = top;
+    circle.style.left = left;
+    circle.style.zIndex = "999999999999999";
+	circle.style.height = size + "px";
+	circle.style.width = size + "px";
 	circle.style.border = "1px solid black";
 	circle.style.borderRadius = "50%";
 	circle.className = "colorlabel_child";
@@ -181,7 +164,7 @@ function addContent (label, content) {
 
 	// if (content.length <= 6 ){
 	// 	console.log(content);
-		textContent.style.fontSize = "18px";
+	textContent.style.fontSize = "18px";
 	textContent.style.paddingTop = "12%";
 	//	textContent.style.paddingLeft= "20%";
 	//	textContent.style.paddingRight = "20%";
@@ -202,41 +185,38 @@ function addContent (label, content) {
 }
 
 function createMinimizedLabel (top, left, label) {
-
-	// circle.style.position = 'relative';
-	// circle.style.margin = "0px";
-
-  // circle.style.top = top;
-  // circle.style.left = left;
-  // circle.style.zIndex = "999999999999999";
-	// circle.style.height = "11.5px";
-	// circle.style.width = "11.5px";
-	// circle.style.border = "1px solid black";
-	// circle.style.borderRadius = "50%";
-	// circle.className = "colorlabel_child";
 	let newMinimizedLabel = document.createElement('div');
 
-	newMinimizedLabel.style.position = 'relative';
-	newMinimizedLabel.style.top = top;
-	newMinimizedLabel.style.left = left;
+	const size = 25
+
+	newMinimizedLabel.style.position = 'absolute';
+	newMinimizedLabel.style.top = top - size/2 + "px" ;
+	newMinimizedLabel.style.left = left - size/2 + "px" ;
 	newMinimizedLabel.style.borderRadius = "40%";
 	newMinimizedLabel.style.zIndex = "999999999999999";
 	newMinimizedLabel.style.background = "#000000";
 	newMinimizedLabel.style.cursor  = "pointer";
 	newMinimizedLabel.style.display = "none"
+	newMinimizedLabel.style.textAlign = "center";
+	newMinimizedLabel.style.verticalAlign = "middle";
+	newMinimizedLabel.style.width = "25px";
+	newMinimizedLabel.style.height = "25px";
 
 	newMinimizedLabel.className = "colorlabel";
 
 	let maximizeButton = document.createElement("p");
 
-	maximizeButton.style.float = "left";
+	// maximizeButton.style.float = "left";
+	//maximizeButton.style.display ="inline-block;";
 	maximizeButton.style.color = "#ffffff";
-	maximizeButton.style.fontSize = "25px";
-	maximizeButton.style.marginTop = "0px";
-	maximizeButton.style.marginBottom = "0px";
-	maximizeButton.style.padding = "5px";
-
+	maximizeButton.style.fontSize = "20px";
+	// maximizeButton.style.marginTop = "0px";
+	// maximizeButton.style.marginBottom = "0px";
+	// maximizeButton.style.padding = "5px";
+	maximizeButton.style.width = newMinimizedLabel.style.width
+	maximizeButton.style.height = newMinimizedLabel.style.height
 	maximizeButton.style.fontFamily = "Arial,Calibri,sans-serif";
+	//maximizeButton.style.fontWeight = "normal";
 	
 	maximizeButton.className = "colorlabel_child";
 	maximizeButton.onclick = () => {toggleLabel(newMinimizedLabel, label)}
@@ -298,13 +278,13 @@ function createStatusLabel (content) {
 }
 
 function addStatusLabel () {
-  statusLabelOn.style.display = "block";
-  statusLabelOff.style.display = "none";
+    statusLabelOn.style.display = "block";
+    statusLabelOff.style.display = "none";
 }
 
 function removeStatusLabel () {
-  statusLabelOn.style.display = "none";
-  statusLabelOff.style.display = "block";
+    statusLabelOn.style.display = "none";
+    statusLabelOff.style.display = "block";
 	setTimeout(() => { statusLabelOff.style.display = "none"; }, 2000);
 }
 
@@ -324,5 +304,5 @@ function setupStatusLabel () {
 	});
 
 	document.body.appendChild(statusLabelOn);
-  document.body.appendChild(statusLabelOff);
+    document.body.appendChild(statusLabelOff);
 }
